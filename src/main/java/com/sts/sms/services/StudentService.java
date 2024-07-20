f0e73d162666065239685af9ac6564b1beb27fd2
faiza <faiza@Faizan>
2024-07-20 16:44:12 +0530

    First commit

---
+++ src/main/java/com/sts/sms/services/StudentService.java
@@ -0,0 +1,18 @@
+package com.sts.sms.services;
+
+import com.sts.sms.Entity.*;
+import java.util.List;
+
+
+
+public interface StudentService {
+	List<Student> getAllStudents();
+	
+	Student saveStudent(Student student);
+	
+	Student getStudentById(Long id);
+	
+	Student updateStudent(Student student);
+	
+	void deleteStudentById(Long id);
+}
